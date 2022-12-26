8f9647f1a4a892bdfd054c18fd71955874134d18
Amol Deshmukh <amol.deshmukh@idmission.com>
2022-12-26 21:55:12 +0530

    First Commit

---
+++ AesGcmNoPadding.framework/Headers/AesGcmNoPadding.h
@@ -0,0 +1,18 @@
+//
+//  AesGcmNoPadding.h
+//  AesGcmNoPadding
+//
+//  Created by Amol Deshmukh on 24/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AesGcmNoPadding.
+FOUNDATION_EXPORT double AesGcmNoPaddingVersionNumber;
+
+//! Project version string for AesGcmNoPadding.
+FOUNDATION_EXPORT const unsigned char AesGcmNoPaddingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AesGcmNoPadding/PublicHeader.h>
+
+
